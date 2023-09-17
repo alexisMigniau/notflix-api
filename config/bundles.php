4971ef4bdeeca97cd572f3181700a499b5dcf4f8
Alexis Migniau <migniau.alexis@gmail.com>
2023-09-18 01:36:46 +0200

    feat(user) : Ajout refresh token

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
